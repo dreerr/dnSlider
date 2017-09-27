a11d6e57fccc96935cd5e373bc7722ee58626934
Diktus Dreibholz <dreerr@gmail.com>
2017-09-27 09:19:27 +0200

    Rewrite with new option `data-async`

--- dn-slider.js
+++ dn-slider.js
@@ -1,43 +1,44 @@
 /*! https://github.com/dreerr/dnSlider
    v1.1 | 20170923
    License: All rights reserved
-*/
+   */
 
-(function($) {
-  $.dnSlider = function(element, options) {
-    let defaults = {
-      min: 0,
-      max: 100,
-      step: 1,
-      scroll: true,
-      scrollTargetClass: '.year-container',
-      scrollTargetAttr: 'data-year',
-      orientation: 'bottom',
-      scrollTop: 80,
-      highlight: false,
-      debug: false,
-      onChange(e) {}
-    };
+   (function($) {
+    $.dnSlider = function(element, options) {
+      let defaults = {
+        min: 0,
+        max: 100,
+        step: 1,
+        scroll: true,
+        scrollTargetClass: '.year-container',
+        scrollTargetAttr: 'data-year',
+        orientation: 'bottom',
+        scrollTop: 80,
+        highlight: false,
+        async: false,
+        debug: false,
+        onChange(e) {}
+      };
 
-    let plugin = this;
-    plugin.settings = {};
-    let $element = $(element);
-    let $sliderInner = $('<div class="slider-inner"/>');
-    let $ruler = $('<div class="slider-ruler" unselectable="on"/>');
-    let $handle = $('<div class="slider-handle" />');
-    let pressed = false;
-    let animating = false;
+      let plugin = this;
+      plugin.settings = {};
+      let $element = $(element);
+      let $sliderInner = $('<div class="slider-inner"/>');
+      let $ruler = $('<div class="slider-ruler" unselectable="on"/>');
+      let $handle = $('<div class="slider-handle" />');
+      let pressed = false;
+      let animating = false;
 
-    plugin.init = function() {
-      plugin.settings = $.extend({}, defaults, options, $element.data());
-      log(plugin.settings);
-      $element.addClass(plugin.settings.orientation);
-      $handle.appendTo($sliderInner);
-      $sliderInner.appendTo($element);
-      plugin.reloadScrollItems();
-      appendRuler();
-      plugin.value = plugin.settings.min;
-      setActiveLabel();
+      plugin.init = function() {
+        plugin.settings = $.extend({}, defaults, options, $element.data());
+        log(plugin.settings);
+        $element.addClass(plugin.settings.orientation);
+        $handle.appendTo($sliderInner);
+        $sliderInner.appendTo($element);
+
+      plugin.reloadScrollItems(); // WILL INIT ALL
+
+      // HANDLERS
       $element.down(function(e) {
         $('html,body').addClass('no-select');
         pressed=true;
@@ -49,11 +50,17 @@
         updateHandle(e);
       });
       $(window).resize(() => updateHandle());
-      if (plugin.settings.scroll) { watchPageScroll(); }
+      if (plugin.settings.scroll) {
+        $(window).scroll(() => watchPageScroll());
+      }
     };
 
     plugin.reloadScrollItems = function() {
       plugin.scrollItems = $(plugin.settings.scrollTargetClass+':visible');
+      updateRuler();
+      plugin.value = plugin.items[0];
+      setActiveLabel();
+      watchPageScroll();
     }
 
     plugin.setValue = function(value) {
@@ -82,9 +89,9 @@
       }
     };
 
-    var setValueByAmount = function(amount, propagate) {
-      let range = Math.abs(Math.abs(plugin.settings.min) - Math.abs(plugin.settings.max));
-      let value = Math.round(plugin.settings.min + (range*plugin.settings.step*amount));
+    var setValueByAmount = function(amount) {
+      let pos = Math.round(amount*(plugin.items.length-1));
+      let value = plugin.items[pos];
       if (value!==plugin.value) {
         plugin.value = value;
         setActiveLabel();
@@ -93,8 +100,7 @@
     };
 
     var updateHandle = function() {
-      let range = Math.abs(Math.abs(plugin.settings.min) - Math.abs(plugin.settings.max));
-      let amount = Math.abs(plugin.value-plugin.settings.min)/range;
+      let amount = plugin.items.indexOf(plugin.value)/(plugin.items.length-1);
       if (plugin.settings.orientation=='bottom') {
         $handle.css('left', (amount*$sliderInner.width())+'px');
       } else {
@@ -124,53 +130,54 @@
           animating = true;
           $('html,body').animate(
             {scrollTop: target.offset().top - plugin.settings.scrollTop}
-          , 1000, () => animating = false);
+            , 1000, () => animating = false);
         }
         , 500);
       }
     };
 
     var watchPageScroll = function() {
-      plugin.reloadScrollItems();
       if (plugin.scrollItems.length) {
-        $(window).scroll(function() {
-          if (!pressed && !animating) {
-            let fromTop = $(this).scrollTop();
-            let cur = plugin.scrollItems.map(function() {
-              if($(this).offset().top >= fromTop) {
-                return this;
-              }
-            });
-            if (cur.length===0) { cur = plugin.scrollItems.last(); }
-            let scroll_value=cur.attr(plugin.settings.scrollTargetAttr);
-            plugin.setValue(scroll_value);
-          }
-        });
+        if (!pressed && !animating) {
+          let fromTop = $(this).scrollTop();
+          let cur = plugin.scrollItems.map(function() {
+            if($(this).offset().top + $(this).height() >= fromTop) {
+              return this;
+            }
+          });
+          if (cur.length===0) { cur = plugin.scrollItems.last(); }
+          let scroll_value=cur.attr(plugin.settings.scrollTargetAttr);
+          plugin.setValue(scroll_value);
+        }
       }
     };
 
-    var appendRuler = function() {
+    var updateRuler = function() {
       let range = "";
-      let highlightItems = [];
-      if (plugin.settings.scroll && plugin.settings.highlight) {
-        $sliderInner.addClass('highlight-active');
-        highlightItems = plugin.scrollItems.map(function() {
-          return $(this).attr(plugin.settings.scrollTargetAttr);
-        });
+      plugin.items = [];
+      let highlightItems = plugin.scrollItems.map(function() {
+        return $(this).attr(plugin.settings.scrollTargetAttr);
+      }).toArray();
+      if (plugin.settings.highlight) {
+        $element.addClass('highlight-all');
       }
-      let numElems = Math.floor((plugin.settings.max - plugin.settings.min)/plugin.settings.step+1)-1;
-      let value = plugin.settings.min;
-      idx=0;
-      while (plugin.settings.step > 0 ? value<=plugin.settings.max : value>= plugin.settings.max) {
-        let makeHighlight = Array.from(highlightItems).includes(value.toString()) ? ' class="highlight"' : '';
-        let segment = 100.0/numElems*idx;
+      if (plugin.settings.async) {
+        plugin.items = highlightItems;
+        $element.addClass('show-all');
+      } else {
+        let value = plugin.settings.min;
+        while (plugin.settings.step > 0 ? value<=plugin.settings.max : value>= plugin.settings.max) {
+          plugin.items.push(value.toString());
+          value += plugin.settings.step;
+        }
+      }
+      $(plugin.items).each(function(index, value) {
+        let makeHighlight = highlightItems.indexOf(value)!==-1 ? ' class="highlight"' : '';
+        let segment = 100.0/(plugin.items.length-1)*index;
         let pos = `${plugin.settings.orientation=="bottom" ? "left":"top"}: ${segment}%`;
         range += `<span data-value='${value}'${makeHighlight} style='${pos}'></span>`;
-        value += plugin.settings.step;
-        idx++;
-      }
+      });
       $ruler.html(range);
-      let size = 100.0 / ($ruler.children().length -1);
       $sliderInner.prepend($ruler);
     };
 
